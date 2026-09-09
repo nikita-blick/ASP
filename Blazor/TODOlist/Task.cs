50cb09bb6a95bfdad2ab950a6f94465f1cbf8367
Nikita <Nikita@DESKTOP-13IEN5T>
2026-09-09 17:29:24 +0500

    TaskList DONE

---
+++ Blazor/TODOlist/Task.cs
@@ -0,0 +1,17 @@
+﻿namespace TODOlist
+{
+	public class Task
+	{
+		public string Description { get; set; }
+		public bool DONE { get; set; }
+		public DateTime AssignTime { get; set; }
+		public override bool Equals(object? other)
+		{
+			return this.Description.Equals
+				(
+					(other as TODOlist.Task).Description,
+					StringComparison.OrdinalIgnoreCase
+				);
+		}
+	}
+}
