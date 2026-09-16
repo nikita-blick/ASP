b2f7eded59d3fb5f998ea99976051971f6037103
Nikita <Nikita@DESKTOP-13IEN5T>
2026-09-16 21:49:22 +0500

    Groups & Direction Nabigation properties

---
+++ Blazor/Academy/Data/AcademyContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Academy.Models;
+
+namespace Academy.Data
+{
+    public class AcademyContext : DbContext
+    {
+        public AcademyContext (DbContextOptions<AcademyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Academy.Models.Discipline> Disciplines { get; set; } = default!;
+        public DbSet<Academy.Models.Direction> Directions { get; set; } = default!;
+        public DbSet<Academy.Models.Group> Groups { get; set; } = default!;
+    }
+}
---
+++ Blazor/Academy/Models/Discipline.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Academy.Models
+{
+	public class Discipline
+	{
+		[Key]
+		[Column(TypeName = "SMALLINT")]
+		public int discipline_id {  get; set; }
+
+		[Required]
+		public string discipline_name { get; set; }
+
+		[Required]
+		[Column(TypeName = "TINYINT")]
+		public int number_of_lessons { get; set; }
+	}
+}
