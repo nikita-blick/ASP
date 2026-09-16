e1bc6fe799bf1a66f5a802d72182dba2f624d7c1
Nikita <Nikita@DESKTOP-13IEN5T>
2026-09-16 23:45:31 +0500

    Student in progress

--- Blazor/Academy/Models/Human.cs
+++ Blazor/Academy/Models/Human.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Academy.Models
 {
@@ -18,6 +19,13 @@ namespace Academy.Models
 		public string? email { get; set; }
 		[Phone]
 		public string? phone { get; set; }
+
+		[Column("photo", TypeName = "IMAGE")]
 		public byte[]? photo { get; set; }	
+		// Calculated properties:
+		public string FullName
+		{
+			get => $"{last_name} {first_name} {middle_name}";
+		}
 	}
 }
--- Blazor/Academy/Models/Student.cs
+++ Blazor/Academy/Models/Student.cs
@@ -1,13 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Academy.Models
 {
-	public class Student
+	public class Student:Human
 	{
 		[Key]
 		public int stud_id { get; set; }
 
 		[Required]
+		[ForeignKey(nameof(Group))]
 		public int group { get; set; }
+
+		//			Navigation properties:
+		public Group Group { get; set; }
 	}
 }
